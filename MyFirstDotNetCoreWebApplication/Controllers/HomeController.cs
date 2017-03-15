fb86d37d8ffd23c8a3d8cd8c3ab14cb6f6f3de77
Simone Grignola <sito@grignola.ch>
2017-03-16 00:33:14 +0100

    Added post deploy parameters

--- MyFirstDotNetCoreWebApplication/Controllers/HomeController.cs
+++ MyFirstDotNetCoreWebApplication/Controllers/HomeController.cs
@@ -1,15 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 
 namespace MyFirstDotNetCoreWebApplication.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly EnvironmentConfig _configuration;
+
+        public HomeController(IOptions<EnvironmentConfig> configuration)
+        {
+            _configuration = configuration.Value;
+        }
+
         public IActionResult Index()
         {
+            ViewData["Weight"] = _configuration.Weight;
+            ViewData["Time"] = _configuration.Time;
+            ViewData["Height"] = _configuration.Height;
             return View();
         }
 
--- MyFirstDotNetCoreWebApplication/Startup.cs
+++ MyFirstDotNetCoreWebApplication/Startup.cs
@@ -29,6 +29,7 @@ namespace MyFirstDotNetCoreWebApplication
         {
             // Add framework services.
             services.AddMvc();
+            services.Configure<EnvironmentConfig>(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -56,5 +57,14 @@ namespace MyFirstDotNetCoreWebApplication
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
         }
+    }
+
+    public class EnvironmentConfig
+    {
+        public string Time { get; set; }
+
+        public string Weight { get; set; }
+
+        public string Height { get; set; }
     }
 }
